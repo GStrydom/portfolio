ffb61de5f4e2c99b4414921f9377f4deb5a882cb
Gregory Strydom <gregory.strydom079@gmail.com>
2020-11-19 16:32:21 +0200

    Fixes
    Removed redundant Button import from Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import { Button as SemanticButton} from 'semantic-ui-react'
 import NewsForm from './NewsForm';
